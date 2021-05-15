01c5d1091edd9ba9256bd2b200316bdbd366ddaf
Neil Walker <neil@wynded.co.uk>
2021-05-15 16:15:48 +0100

    Dump all tracks from the db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pathlib
 from sqlalchemy import select
 
 from database import Database
-from schema import Album
+from schema import Album, Track
 # from scan_mp3 import scan_mp3
 from scan_m4a import scan_m4a
 
@@ -17,24 +17,29 @@ def main():
     #     track = scan_mp3(path)
     #     print(track)
     for path in basedir.rglob('*.m4a'):
-        print(path)
         track, albumref = scan_m4a(path)  # These are pure object models - no ids or cross references yet
         # fill in ids and cross-references
         db.track(track)  # updates track.Id
         track.Album = db.album(albumref)  # also updates albumref.Id
-        print("  Album %s (%s==%s): Track %s" % (albumref.Title, track.Album, albumref.Id, track.Title))
         albumref.Tracks.append(track)
 
     # dump all albums
     print("Albums:")
     albums = db.session.execute(select(Album).order_by(Album.Artist, Album.Title))
     for album in albums.scalars().all():
-        print("Artist: '%s', Title: '%s', Id: '%s'" % (album.Artist, album.Title, album.Id))
+        print("Artist: '%s', Title: '%s', Id: %s" % (album.Artist, album.Title, album.Id))
         print("  Tracks:")
         for track in sorted(album.Tracks, key=lambda t: -1 if (t.TrackNumber is None) else t.TrackNumber):
             print("    %s: %s (%s)" % (track.TrackNumber, track.Title, track.Filepath))
+    print("\n\n")
 
     # and all tracks
+    print("Tracks:")
+    tracks = db.session.execute(select(Track).order_by(Track.Artist, Track.Album, Track.TrackNumber))
+    for track in tracks.scalars().all():
+        print("Artist: '%s', Album id: %s, Track number: %s, Track title: '%s', Genre: '%s'" % (
+            track.Artist, track.Album, track.TrackNumber, track.Title, track.Genre))
+    print("\n\n")
 
 
 if __name__ == '__main__':
