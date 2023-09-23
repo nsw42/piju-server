a0e4f82120e9531361a835051c21057c38c22ea0
Neil Walker <neil@wynded.co.uk>
2023-09-23 11:43:14 +0100

    Don't rely on PATH to find alembic

--- run.sh
+++ run.sh
@@ -37,14 +37,25 @@ fi
 
 DB_FILE=`realpath "$DB_FILE"`
 
+# Check for alembic
+ALEMBIC=$(which alembic)
+if [ -z "$ALEMBIC" ]; then
+  # Try in the default installation
+  ALEMBIC=$HOME/.local/bin/alembic
+  if [ ! -x "$ALEMBIC" ]; then
+    echo alembic not found. Aborting.
+    exit 1
+  fi
+fi
+
 # Update database schema as necessary
 export DB_FILE
 cd $SCRIPT_DIR/pijuv2/database
-alembic current 2> /dev/null | grep -q head && { echo Database scehma is up-to-date; } || {
+$ALEMBIC current 2> /dev/null | grep -q head && { echo Database scehma is up-to-date; } || {
   BACKUP=${DB_FILE}.bak
   echo "Updating database file: keeping a backup as $BACKUP"
   cp -pf "$DB_FILE" "$BACKUP"
-  alembic upgrade head || { echo Database upgrade failed. Aborting.; exit 1; }
+  $ALEMBIC upgrade head || { echo Database upgrade failed. Aborting.; exit 1; }
 }
 cd $SCRIPT_DIR
 
