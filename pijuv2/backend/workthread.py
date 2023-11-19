d2473148b0c219dce8ab25fa9e9ecc8287c38f0f
Neil Walker <neil@wynded.co.uk>
2023-11-19 17:08:34 +0000

    Ensure app context in worker thread for flask-sqlalchemy

--- pijuv2/backend/workthread.py
+++ pijuv2/backend/workthread.py
@@ -23,28 +23,29 @@ class WorkerThread(threading.Thread):
             self.current_status = 'Idle'
             request = self.work_queue.get()
 
-            with DatabaseAccess() as db:
-                if request[0] == WorkRequests.SCAN_DIRECTORY:
-                    dir_to_scan = pathlib.Path(request[1])
-                    self.current_status = f'Scanning {dir_to_scan}'
-                    scan_directory(dir_to_scan, db)
+            with self.app.app_context():
+                with DatabaseAccess() as db:
+                    if request[0] == WorkRequests.SCAN_DIRECTORY:
+                        dir_to_scan = pathlib.Path(request[1])
+                        self.current_status = f'Scanning {dir_to_scan}'
+                        scan_directory(dir_to_scan, db)
 
-                elif request[0] == WorkRequests.DELETE_MISSING_TRACKS:
-                    self.current_status = 'Deleting missing tracks'
-                    delete_missing_tracks(db)
+                    elif request[0] == WorkRequests.DELETE_MISSING_TRACKS:
+                        self.current_status = 'Deleting missing tracks'
+                        delete_missing_tracks(db)
 
-                elif request[0] == WorkRequests.DELETE_ALBUMS_WITHOUT_TRACKS:
-                    self.current_status = 'Deleting albums without tracks'
-                    delete_albums_without_tracks(db)
+                    elif request[0] == WorkRequests.DELETE_ALBUMS_WITHOUT_TRACKS:
+                        self.current_status = 'Deleting albums without tracks'
+                        delete_albums_without_tracks(db)
 
-                elif request[0] == WorkRequests.FETCH_FROM_YOUTUBE:
-                    url = request[1]
-                    download_dir = request[2]
-                    self.current_status = f'Fetching {url} to {download_dir}'
-                    local_files = fetch_audio(url=url, download_dir=download_dir)
-                    callback = request[3]
-                    if callback:
-                        callback(self.app, url, local_files)
+                    elif request[0] == WorkRequests.FETCH_FROM_YOUTUBE:
+                        url = request[1]
+                        download_dir = request[2]
+                        self.current_status = f'Fetching {url} to {download_dir}'
+                        local_files = fetch_audio(url=url, download_dir=download_dir)
+                        callback = request[3]
+                        if callback:
+                            callback(self.app, url, local_files)
 
-                else:
-                    logging.error(f"Unrecognised request: {request[0]}")
+                    else:
+                        logging.error(f"Unrecognised request: {request[0]}")
